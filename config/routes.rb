631f34846812616f6a1adea7c9d93f3554586958
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-01-12 20:11:12 +0100

    Added User and Shout page

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
-  resources :users, only: [:create] do
+  resources :users, only: [:create, :show] do
     resource :password,
       controller: "clearance/passwords",
       only: [:edit, :update]
   end
 
+  resources :shouts, only: [:create, :show]
+  
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
