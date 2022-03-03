ac1c3fdce6d645a6b3a97ae69bc109ed09c94b89
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-03-03 16:30:26 +0100

    Add autolink functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < Clearance::UsersController
   def show
-    @user = User.find(params[:id])
-    @shouts = @user.shouts
+    @user = User.find_by_username(params[:id])
+    @user? @shouts = @user.shouts : redirect
   end
 
   private
 
   def user_params
     params.require(:user).permit(:email, :password, :username)
+  end
+
+  def redirect
+    redirect_to root_path, alert: "User not found..."
   end
 end
--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -13,4 +13,8 @@ module ShoutHelper
       link_to "like", like_shout_path(shout), method: :post, class: "like-padding-left"
     end
   end
+
+  def auto_link(text)
+    text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1])}.html_safe
+  end
 end
