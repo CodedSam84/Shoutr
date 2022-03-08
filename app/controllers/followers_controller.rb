664648b891bafdd4872dfd92302043d6ba9b15c3
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-03-08 12:30:28 +0100

    Add followers relationship

---
+++ app/controllers/followers_controller.rb
@@ -0,0 +1,13 @@
+class FollowersController < ApplicationController
+  def index
+    @user = find_user
+    @followers = @user.followers
+  end
+
+  private
+
+  def find_user
+    @user ||= User.find_by_username(params[:user_id])
+  end
+
+end
