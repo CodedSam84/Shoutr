017d119562d061dcd4c92e9faa8bbbffa7d9b690
Samuel Atobatele <swumatob@yahoo.co.uk>
2022-03-16 14:14:47 +0100

    Allow for searching photoshouts

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -4,12 +4,35 @@ class ShoutSearchQuery
   end
 
   def to_relation
-    Shout.
-      joins("INNER JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").
-      where("body ILIKE ?", "%#{term}%")
+    matching_shouts_for_text_shout.
+      or(matching_shouts_for_photo_shout)
   end
 
   private
 
   attr_reader :term
+
+  def matching_shouts_for_text_shout
+    Shout.where(content_type: "TextShout", content_id: matching_text_shouts)
+  end
+
+  def matching_shouts_for_photo_shout
+    Shout.where(content_type: "PhotoShout", content_id: matching_photo_shouts)
+  end
+
+  def matching_text_shouts
+    TextShout.where("body ILIKE ?", "%#{term}%").select(:id)
+  end
+
+  def matching_photo_shouts
+    photo_shouts_joins_with_active_storage.where("filename ILIKE ?", "%#{term}%").select(:id)
+  end
+
+  def photo_shouts_joins_with_active_storage
+    PhotoShout.
+      joins("INNER JOIN active_storage_attachments ON 
+      record_id = photo_shouts.id INNER JOIN active_storage_blobs ON 
+      active_storage_blobs.id = blob_id")
+  end
+
 end
